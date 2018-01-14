5278d3002223b7d28df5e16126b75ff277a803e8
ethan375 <ethanebel1@gmail.com>
2018-01-13 19:39:20 -0600

    created new page with form as well as the route

--- controller/carController.js
+++ controller/carController.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-  res.send('pathworking')
-})
+  res.send('pathworking');
+});
+
+router.get('/new', (req,res)=>{
+  res.render('../views/new.ejs');
+});
 
 
 
