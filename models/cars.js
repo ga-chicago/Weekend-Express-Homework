e734c816de1df8466c17e04ecc5a5546343613b2
ethan375 <ethanebel1@gmail.com>
2018-01-13 22:45:52 -0600

    created car schema

---
+++ models/cars.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+  make:{type:String, require:true},
+  model:{type:String, require:true},
+  year{type:String, require:true},
+  color:{type:String, required:true}
+});
+
+// create the model
+const Car = mongoose.model('Car', carSchema);
+
+// export
+module.exports = Car;
