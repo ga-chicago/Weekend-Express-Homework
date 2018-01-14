4f2ab5ea38b78b4a7dd2e87b93fcc894513276af
ethan375 <ethanebel1@gmail.com>
2018-01-14 11:58:39 -0600

    index route communicating with db

--- controller/carController.js
+++ controller/carController.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const Car = require('../models/cars.js')
 
 router.get('/', (req,res)=>{
-  res.render('index.ejs');
+  // search the database
+  Car.find({}, (err, allCars)=>{
+    if(err)console.log(err);
+    // console.log(allCars);
+    res.render('index.ejs', {cars: allCars});
+  });
 });
+
+
 
 router.get('/new', (req,res)=>{
   res.render('../views/new.ejs');
@@ -14,7 +21,7 @@ router.post('/', (req,res)=>{
   Car.create(req.body, (err, createdCar)=>{
   // res.send(createdCar);
   console.error(err);
-  res.redirect('localhost:3000/cars');
+  res.redirect('/cars');
   });
 });
 
