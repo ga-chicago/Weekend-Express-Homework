b774a824ebc89164a9db00c6c73e6827908a6715
ethan375 <ethanebel1@gmail.com>
2018-01-13 19:25:14 -0600

    created file structure and connected db

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-
+require('./db/db.js')
 
 
 
 app.listen(3000, ()=>{
-  console.log("Server listening on port 3000")
-})
+  console.log("Server listening on port 3000");
+});
