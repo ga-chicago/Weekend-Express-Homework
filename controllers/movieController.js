b6a7a26b66a90bb7f22d4a0f0ee9d0a8d12fcc48
Anthony <anthonyjlower@gmail.com>
2018-01-13 19:46:37 -0600

    Created new template that displays a form for a user to submit

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,11 +3,20 @@ const router = express.Router();
 const Movie = require("../models/movie.js")
 
 
+// Movies Route
 router.route("/")
 	.get((req, res) => {
 		res.send("Hit index route");
 		console.log("Hit index route");
 	})
+
+
+// New Route
+router.route("/new")
+	.get((req, res) => {
+		res.render("new.ejs")
+	})
+
 
 
 
