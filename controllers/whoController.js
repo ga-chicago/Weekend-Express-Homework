bcadb0fa2758d722ed90d99e757d7c3fbfa93963
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-16 13:38:30 -0600

    did the chaining of routes properly this time

--- controllers/whoController.js
+++ controllers/whoController.js
@@ -9,28 +9,29 @@ const Whovian = require('../models/who.js');
 
 // INDEX ROUTE
 // POST ROUTE (for the new characters)
-router.get('/', (req, res) => {
-	Whovian.find({}, (err, allWho) => {
-		if(err) console.log(err);
-		console.log('hit index route')
-		// res.send('hit index route');
-		res.render('index.ejs', {whovians: allWho})
+router.route('/')
+	.get((req, res) => {
+		Whovian.find({}, (err, allWho) => {
+			if(err) console.log(err);
+			console.log('hit index route')
+			// res.send('hit index route');
+			res.render('index.ejs', {whovians: allWho})
+		})
+	}).post((req, res) => {
+		// update the friend field to actually be a boolean
+		// console.log(req.body)
+		if(req.body.friend === 'on') {
+			req.body.friend = true;
+		} else {
+			req.body.friend = false;
+		}
+		// created the character in the database when the user submits the form
+		Whovian.create(req.body, (err, createdWhovian) => {
+			if(err) console.log(err);
+			console.log('hit post route')
+			res.redirect('/whovians')
 	})
-}).post('/', (req, res) => {
-	// update the friend field to actually be a boolean
-	// console.log(req.body)
-	if(req.body.friend === 'on') {
-		req.body.friend = true;
-	} else {
-		req.body.friend = false;
-	}
-	// created the character in the database when the user submits the form
-	Whovian.create(req.body, (err, createdWhovian) => {
-		if(err) console.log(err);
-		console.log('hit post route')
-		res.redirect('/whovians')
-	})
-	// res.send('hit post route')
+		// res.send('hit post route')
 })
 
 // NEW ROUTE (give the user the form to add characters)
@@ -41,30 +42,31 @@ router.get('/new', (req, res) => {
 // SHOW ROUTE
 // UPDATE - PUT ROUTE
 // DELETE ROUTE
-router.get('/:id', (req, res) => {
-	// get the information on that character
-	Whovian.findById(req.params.id, (err, foundWhovian) => {
-		if(err) console.log(err);
-		res.render('show.ejs', { whovian: foundWhovian})
-	})
-}).put('/:id', (req, res) => {
-	// create boolean in friend field
-	if(req.body.friend === 'on') {
-		req.body.friend = true;
-	} else {
-		req.body.friend = false;
-	}
-	// copying what Jim did in class and just passing req.body as the new object! (I typed it all out before)
-	Whovian.findByIdAndUpdate(req.params.id, 
-		req.body, 
-		(err, updatedModel) => {
+router.route('/:id')
+	.get((req, res) => {
+		// get the information on that character
+		Whovian.findById(req.params.id, (err, foundWhovian) => {
+			if(err) console.log(err);
+			res.render('show.ejs', { whovian: foundWhovian})
+		})
+	}).put((req, res) => {
+		// create boolean in friend field
+		if(req.body.friend === 'on') {
+			req.body.friend = true;
+		} else {
+			req.body.friend = false;
+		}
+		// copying what Jim did in class and just passing req.body as the new object! (I typed it all out before)
+		Whovian.findByIdAndUpdate(req.params.id, 
+			req.body, 
+			(err, updatedModel) => {
+				if(err) console.log(err);
+				res.redirect('/whovians');
+		})
+	}).delete((req, res) => {
+		Whovian.findByIdAndRemove(req.params.id, (err, response) => {
 			if(err) console.log(err);
 			res.redirect('/whovians');
-	})
-}).delete('/:id', (req, res) => {
-	Whovian.findByIdAndRemove(req.params.id, (err, response) => {
-		if(err) console.log(err);
-		res.redirect('/whovians');
 	})
 })
 
